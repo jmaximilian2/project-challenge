6d7bfe337d87268054957fe10399027002689a97
Maximilian Jelinek <maximilian@jelinek.dev>
2020-02-12 12:39:05 +0100

    feat: add cross origin header for localhost frontend

--- search-requests/backend/src/main/java/searchrequests/controller/SearchRequestController.java
+++ search-requests/backend/src/main/java/searchrequests/controller/SearchRequestController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/requests")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SearchRequestController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
     private final SearchRequestService searchRequestService;
