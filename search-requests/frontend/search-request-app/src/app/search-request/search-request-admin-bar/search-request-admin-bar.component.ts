6824bcdce6b9a638e8b44880fb3b5d674561432e
Maximilian Jelinek <maximilian@jelinek.dev>
2020-02-12 13:45:16 +0100

    feat: added search request panel showing count indicator and add button to separate component

---
+++ search-requests/frontend/search-request-app/src/app/search-request/search-request-admin-bar/search-request-admin-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-request-admin-bar',
+  templateUrl: './search-request-admin-bar.component.html',
+  styleUrls: ['./search-request-admin-bar.component.scss']
+})
+export class SearchRequestAdminBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- search-requests/frontend/search-request-app/src/app/search-request/search-request.module.ts
+++ search-requests/frontend/search-request-app/src/app/search-request/search-request.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RequestCountIndicatorComponent } from './request-count-indicator/request-count-indicator.component';
 import { MatButtonModule } from '@angular/material';
+import { SearchRequestAdminBarComponent } from './search-request-admin-bar/search-request-admin-bar.component';
 
 
 
 @NgModule({
-  declarations: [RequestCountIndicatorComponent],
+  declarations: [RequestCountIndicatorComponent, SearchRequestAdminBarComponent],
   exports: [
-    RequestCountIndicatorComponent
+    RequestCountIndicatorComponent,
+    SearchRequestAdminBarComponent
   ],
   imports: [
     CommonModule,
