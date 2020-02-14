c071a465752ef54ad3c960e97e58bc27dad74ebe
Maximilian Jelinek <maximilian.jelinek@maibornwolff.de>
2020-02-14 08:36:36 +0100

    feat: reset filter on tab change; sticky table header

--- search-requests/frontend/search-request-app/src/app/search-request/search-request-admin-bar/search-request-admin-bar.component.ts
+++ search-requests/frontend/search-request-app/src/app/search-request/search-request-admin-bar/search-request-admin-bar.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { MatDialog } from '@angular/material';
-import { SearchRequestFormComponent } from '../search-request-form/search-request-form.component';
-import { SearchRequest } from '../search-request.model';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {MatDialog} from '@angular/material';
+import {SearchRequestFormComponent} from '../search-request-form/search-request-form.component';
+import {SearchRequest} from '../search-request.model';
 
 @Component({
   selector: 'app-search-request-admin-bar',
@@ -10,13 +10,22 @@ import { SearchRequest } from '../search-request.model';
 })
 export class SearchRequestAdminBarComponent implements OnInit {
   indicatorLabel = 'Placed Search Requests';
+  currentSearchTabIndex = 0;
 
   @Input() requestCount = 0;
   @Output() createRequest = new EventEmitter<SearchRequest>();
   @Output() queryChanged = new EventEmitter<string>();
-  constructor(private dialog: MatDialog) {}
 
-  ngOnInit() {}
+  constructor(private dialog: MatDialog) {
+  }
+
+  ngOnInit() {
+  }
+
+  onTabindexChanged(newIndex: number) {
+    this.currentSearchTabIndex = newIndex;
+    this.onQueryChanged('');
+  }
 
   onQueryChanged(queryString) {
     this.indicatorLabel = queryString && queryString !== '' ? 'Matching Search Request' : 'Placed Search Requests';
@@ -25,7 +34,7 @@ export class SearchRequestAdminBarComponent implements OnInit {
 
   onAddClicked() {
     const dialogRef = this.dialog.open(SearchRequestFormComponent, {
-      data: { title: 'Create Search Request' }
+      data: {title: 'Create Search Request'}
     });
     dialogRef.afterClosed().subscribe((newSearchRequest: SearchRequest) => {
       if (newSearchRequest) {
